e7f5689c110a528ac23f0913fecdd31325b6f989
Ziyang Lin <uoftziyanglin@gmail.com>
2020-08-31 05:53:56 +0000

    finished implementation

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -8,33 +8,59 @@ password=$3
 
 # check if number of arguments is provided correctly
 if [ "$#" -ne 3 ]; then
-  echo "error: illegal number of parameters"
+  echo "error: illegal number of parameters, please input action, username, and password"
   exit
 fi
 
 # check if the docker is started already, and start it if not
-sudo systemctl status docker || systemctl start docker
+sudo systemctl status docker > /dev/null || systemctl start docker
+echo "Docker is ready to use."
 
 case $action in
 'start')
   # if the container does not exist, print error message and exit with code 1
-
-  # start the container
+  if [[ ! $(docker container ls -a -f name=jrvs-psql | grep jrvs-psql) ]]; then
+      echo "error: psql container does not exist, please create a container first."
+      exit 1
+  else
+    # start the container
+    docker container start jrvs-psql > /dev/null
+    echo "container \"jrvs-psql\" started successfully."
+    exit 0
+  fi
   ;;
 
 'stop')
   # if the container does not exist, print error message and exit with code 1
-
-  # stop the container
+  if [[ ! $(docker container ls -a -f name=jrvs-psql | grep jrvs-psql) ]]; then
+      echo "error: psql container does not exist, please create a container first."
+      exit 1
+  else
+    # stop the container
+    docker container stop jrvs-psql > /dev/null
+    echo "container \"jrvs-psql\" stopped successfully."
+    exit 0
+  fi
   ;;
 
 'create')
   # if the container already exist, print error message and exit with code 1
-
-  # create the container with the provided user name and password
+  if [[ $(docker container ls -a -f name=jrvs-psql | grep jrvs-psql) ]]; then
+      echo "error: psql container already exists, please create a container first."
+      exit 1
+  else
+    # create the container with the provided user name and password
+    echo "creating a new psql container..."
+    docker volume create pgdata
+    docker run --name jrvs-psql -e POSTGRES_USER="$2" -e POSTGRES_PASSWORD="$3"  -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+    echo "container created successfully."
+    exit 0
+  fi
   ;;
+
 *)
   # invalid input argument, print error message and exit with code 1
-
+  echo "error: invalid action, please input create, start, or stop."
+  exit 1
   ;;
 esac
