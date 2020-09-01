4bdc04b39cb6f87e19cf977a3c3eb6b2f6e1ee23
Ziyang Lin <uoftziyanglin@gmail.com>
2020-09-01 15:35:24 +0000

    finished ddl.sql

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,27 @@
+/connect host_agent
+
+CREATE TABLE IF NOT EXISTS Public.host_info (
+
+    id SERIAL PRIMARY KEY NOT NULL,
+    hostname VARCHAR UNIQUE NOT NULL,
+    cpu_number INT NOT NULL,
+    cpu_arc VARCHAR NOT NULL,
+    cpu_model VARCHAR NOT NULL,
+    cpu_mhz INT NOT NULL,
+    l2_cache INT NOT NULL,
+    total_mem INT NOT NULL,
+    record_time TIMESTAMP NOT NULL,
+
+);
+
+CREATE TABLE IF NOT EXISTS Public.host_usage (
+
+    host_id INT FOREIGN KEY REFERENCES (host_info.id) NOT NULL,
+    memory_free INT NOT NULL,
+    cpu_idle INT NOT NULL,
+    cpu_kernel INT NOT NULL,
+    disk_io INT NOT NULL,
+    disk_available INT NOT NULL,
+    record_time TIMESTAMP NOT NULL,
+
+);
