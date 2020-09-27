cc2dd31508f77b97dd77da05b5333fe46f229fae
Ziyang Lin <uoftziyanglin@gmail.com>
2020-09-27 03:33:28 +0000

    week 2 coding challenges fix

--- practice/src/test/java/ca/jrvs/practice/codingChallenge/TwoSumTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/TwoSumTest.java
@@ -20,8 +20,8 @@ public class TwoSumTest {
     @Test
     public void twoSumSorted() {
         int[] nums = {1, 2, 3, 6, 11, 13};
-        int[] actual1 = TwoSum.twoSumBruteForce(nums, 7);
-        int[] actual2 = TwoSum.twoSumBruteForce(nums, 10);
+        int[] actual1 = TwoSum.twoSumSorted(nums, 7);
+        int[] actual2 = TwoSum.twoSumSorted(nums, 10);
         assertEquals(0, actual1[0]);
         assertEquals(3, actual1[1]);
         assertEquals(-1, actual2[0]);
@@ -31,8 +31,8 @@ public class TwoSumTest {
     @Test
     public void twoSumHashMap() {
         int[] nums = {1, 2, 3, 6, 11, 13};
-        int[] actual1 = TwoSum.twoSumBruteForce(nums, 7);
-        int[] actual2 = TwoSum.twoSumBruteForce(nums, 10);
+        int[] actual1 = TwoSum.twoSumHashMap(nums, 7);
+        int[] actual2 = TwoSum.twoSumHashMap(nums, 10);
         assertEquals(0, actual1[0]);
         assertEquals(3, actual1[1]);
         assertEquals(-1, actual2[0]);
